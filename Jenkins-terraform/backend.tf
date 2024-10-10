647712c36eb6ecc20371d2c734cf36537a6f25e5
sravani <117513535+sravaninimmi@users.noreply.github.com>
2024-10-10 06:21:12 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sravanii" # Replace with your actual S3 bucket name
+    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
